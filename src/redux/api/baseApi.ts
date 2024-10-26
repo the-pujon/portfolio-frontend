a90fbc7998b1a98cfa59b211a00cf53d2ca5d1ad
Pujon Das Auvi <auvipujondas@gmail.com>
2024-10-27 03:54:22 +0600

    blog api added

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -79,6 +79,7 @@ export const baseApi = createApi({
     "Skill",
     "Education",
     "Experience",
+    "Blog",
   ],
   endpoints: () => ({}),
 });
---
+++ src/redux/features/blog/blogApi.ts
@@ -0,0 +1,60 @@
+import baseApi from "../../api/baseApi";
+
+const blogApi = baseApi.injectEndpoints({
+  endpoints: (builder) => ({
+    // Create blog
+    createBlog: builder.mutation({
+      query: (data) => ({
+        url: "/blog/create",
+        method: "POST",
+        body: data,
+      }),
+      invalidatesTags: ["Blog"],
+    }),
+
+    // Get all blogs
+    getAllBlogs: builder.query({
+      query: () => ({
+        url: "/blog",
+        method: "GET",
+      }),
+      providesTags: ["Blog"],
+    }),
+
+    // Get blog by ID
+    getBlogById: builder.query({
+      query: (id) => ({
+        url: `/blog/${id}`,
+        method: "GET",
+      }),
+      providesTags: ["Blog"],
+    }),
+
+    // Update blog
+    updateBlog: builder.mutation({
+      query: ({ id, data }) => ({
+        url: `/blog/${id}`,
+        method: "PATCH",
+        body: data,
+      }),
+      invalidatesTags: ["Blog"],
+    }),
+
+    // Delete blog
+    deleteBlog: builder.mutation({
+      query: (id) => ({
+        url: `/blog/${id}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: ["Blog"],
+    }),
+  }),
+});
+
+export const {
+  useCreateBlogMutation,
+  useGetAllBlogsQuery,
+  useGetBlogByIdQuery,
+  useUpdateBlogMutation,
+  useDeleteBlogMutation,
+} = blogApi;
